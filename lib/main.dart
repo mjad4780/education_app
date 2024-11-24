c917344670b492c2f0cca2474eda19ff68725256
mjad <mjad3780@gmail.com>
2024-11-24 20:21:44 +0200

    finsh ui and logic all auth and test

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:education/app/education_app.dart';
+import 'package:education/key.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/get_it/get_it.dart';
 import 'core/helpers/cache_helper.dart';
@@ -18,6 +20,8 @@ void main() async {
   //   url: SUPABASE_URL,
   //   anonKey: SUPABASE_ANON_KEY,
   // );
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
+
   runApp(DevicePreview(
       enabled: false, builder: (context) => const EducationApp()));
 }
